16fdfafda3ff6f108420b1385b063a26296a2087
YashVerma24299 <ritikverma23@gmail.com>
2025-02-27 22:47:23 +0530

    Rest function and destructuring

---
+++ 8. Array Method/7. Rest.js
@@ -0,0 +1,37 @@
+//Opposite of Spread
+//take no. of arguments and bundle them 
+
+function sum(...yash){
+    for(let i =0; i<yash.length; i++){
+        console.log("You give:",yash[i]);
+    }
+}
+sum(1,8);
+sum(1,2,3,4);
+
+
+
+//Destructuring
+let names=["tony", "bruce", "steve", "peter", "hela", "loki"];
+let first=names[0];
+let second=names[1];
+console.log(first,second);
+
+let [f, s, other]=names;
+console.log(f,s,other);
+
+let [ff, ss, ...otherr]=names;
+console.log(ff,ss,...otherr);
+
+let student = {
+    firstName: "Yash",
+    lastName: "Verma",
+    age: 20,
+    roll_no: "PCE******7",
+    gender: "male",
+    cgpa: 9.4,
+    color: ["red", "greeen", "blue"],
+};
+
+let {age: A, color:C, city:D="mumbai"}=student;
+console.log(A, C, D);
