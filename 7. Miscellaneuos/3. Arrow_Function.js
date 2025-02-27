efaa0bf2b738af3e59bf658b1d16780993f06c0b
YashVerma24299 <ritikverma23@gmail.com>
2025-02-27 17:00:37 +0530

    Learn about Arrow Function, SetTimeout and SetInterval Function

---
+++ 7. Miscellaneuos/3. Arrow_Function.js
@@ -0,0 +1,37 @@
+    //SYNTAX
+// const fn_name = (arguments1,....) => {//condition}
+
+//more than 1 arg.
+let sum1 = (a,b) => {
+    return a+b;
+}
+console.log(sum1(10,9));
+
+let sum2 = (a,b) => {
+    console.log(a+b);
+}
+sum2(10,9);
+
+//only 1 arg. (No Parenthesis compulsary)
+let cube1 = n => {
+    console.log(n*n*n);
+}
+cube1(5);
+let cube2 = (n) => {
+    console.log(n*n*n);
+}
+cube2(5);
+
+//0 arg. (Parenthesis compulsary)
+let print = () => {
+    console.log("YASH VERMA");
+}
+print();
+
+
+
+    //Implicit return
+//syntax
+//const fn_name = (arg1,...) => (//only return value);
+
+const mul = (a,b) => (a*b);
---
+++ 7. Miscellaneuos/4. SetTimeout_Function.js
@@ -0,0 +1,15 @@
+//setTimeout(function/callback, timeout)
+//1000 = 1second
+console.log("Hi there");
+setTimeout(()=>{
+    console.log("Yash Verma");
+},3000);
+console.log("My name is ");
+
+
+// console.log("Hi there");
+// const f = ()=>{
+//     console.log("Yash Verma");
+// }
+// setTimeout(f,3000);
+// console.log("My name is ");
