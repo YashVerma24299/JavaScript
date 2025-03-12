a8e7ebed8967b27d39200671816af99a079fe75e
YashVerma24299 <ritikverma23@gmail.com>
2025-03-12 12:55:46 +0530

    Understand about the http verbs, status codes and how to add information in URL.

---
+++ 9. DOM/3. Advanced/9. API/5. Http Verbs & Status Codes/script.js
@@ -0,0 +1,21 @@
+                        //http verbs
+// GET
+// POST
+// DELETE
+
+//GET -> Kissi api ya url se information lene kelia isko use karte hai...
+//In hoppscotch -> select get -> put any api/url 
+
+//POST -> Apan api ya url ke saath khuch data bhejna cha-rhe hai...
+
+//DELETE -> data ko delete karne kelia
+
+
+
+
+                        //status codes
+// 200 -> OKAY
+// 404 -> NOT FOUND
+// 400 -> BAD REQUEST
+// 500 -> INTERNAL SERVER ERROR
+// OTHER CAN ALSO READ FROM MDN REFERENCES
---
+++ 9. DOM/3. Advanced/9. API/6. Add Information in URL's/script.js
@@ -0,0 +1,6 @@
+//  https://www.google.com/
+
+//  https://www.google.com/search?q=mango
+//  "key" -> q and "value" -> mango
+
+//  https://www.google.com/search?q=harry+potter
