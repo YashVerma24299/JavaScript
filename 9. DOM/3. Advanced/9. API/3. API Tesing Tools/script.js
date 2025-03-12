aef0c5f22b6851350982971ecae00164e3d9efe3
YashVerma24299 <ritikverma23@gmail.com>
2025-03-12 09:44:18 +0530

    Checkout the API Testing Tools

---
+++ 9. DOM/3. Advanced/9. API/3. API Tesing Tools/script.js
@@ -0,0 +1,7 @@
+//There are two tools
+// 1. Postman -> need to signUp and download as well
+// 2. hoppscotch -> direct use from website
+//we use hoppscotch
+
+//Go -> https://hoppscotch.io/
+//Interact with website
