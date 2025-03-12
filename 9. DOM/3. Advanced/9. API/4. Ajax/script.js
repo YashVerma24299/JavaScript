efad15c386290fe81bda969e9da9c37cde64aafe
YashVerma24299 <ritikverma23@gmail.com>
2025-03-12 09:45:00 +0530

    Asynchronous JavaScript and Xml

---
+++ 9. DOM/3. Advanced/9. API/4. Ajax/script.js
@@ -0,0 +1,13 @@
+//Asynchronous JavaScript and Xml
+
+//user request api
+//api respond user
+//Previously, respond data format is "XML"
+//currently, respond data format is "JSON"
+
+//user request api
+//api respond user
+//Respond jaldi bhi aasakta h aur time bhi lesakta hai... (Asynchronous)
+//jaldi aagya tho okay.
+//Nhi tho, JavaScrit ka code aage chlta rhega aur jab bhi respond aaiga uske saath deal krlegaaa
+
