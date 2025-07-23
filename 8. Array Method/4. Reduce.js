6766e1fd79cb833bf01abfee9d8b91ce12dd8c1a
Yash <ritikverma23@gmail.com>
2025-07-23 21:14:26 +0530

    Implement reduce() for sum, max, and product of array.

--- 8. Array Method/4. Reduce.js
+++ 8. Array Method/4. Reduce.js
@@ -1,4 +1,12 @@
-//syntax: arr.reduce(accumulator,ele)
+// array.reduce((accumulator, currentValue) => {
+//     // return something
+// }, initialValue);
+
+// accumulator → har step pe value store krta hai.
+// currentValue (or ele) → abhi jo array ka element process ho raha hai.
+// initialValue (optional) → starting value. Agar na do, toh accumulator becomes the first element, and currentValue becomes the second.
+
+
 let num= [1,2,3,4,5,6];
 
 let sum=num.reduce((return_value_of_everytime_store_here,ele) => {
