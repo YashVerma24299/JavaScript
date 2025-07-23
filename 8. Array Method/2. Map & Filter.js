3582067152a1c67902714bf878e4aae429ff9bec
Yash <ritikverma23@gmail.com>
2025-07-23 21:12:22 +0530

    Implemented the map and filter operation.

--- 8. Array Method/2. Map & Filter.js
+++ 8. Array Method/2. Map & Filter.js
@@ -37,3 +37,27 @@ let res=num1.filter( (i) =>{
     return i%2==0;
 });
 console.log(res);
+
+
+
+// 🔹 map():
+// Returns a new array where each element is "transformed" based on a callback.              important
+// Always returns an array with the same length as the original array.
+
+// 🔹 filter():
+// Returns a new array with "only the elements that pass the truthy test".                    important
+// The length may be less than or equal to the original array
+
+
+// 1. Square all even numbers in an array
+            // Wrong code
+// let arr = [1, 2, 3, 4, 5, 6];
+// let result = arr.filter((i)=>{
+//     if( i%2==0 && (i*i)%2==0) return i*i;
+// })
+// console.log(result);
+            // Corrected Code
+// let arr = [1, 2, 3, 4, 5, 6];
+// let result = arr.filter(num => num % 2 === 0).map(num => num * num);
+// number jab even hoga tbhi ye aaage bhadke map ko call krpaaiga
+// console.log(result); // [4, 16, 36]
